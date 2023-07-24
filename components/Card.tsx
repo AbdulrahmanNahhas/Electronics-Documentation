4d42190010c9d0b373a0412027f0f2e7256d37a7
Abdulrahman Nahhas <91866837+AbdulrahmanNahhas@users.noreply.github.com>
2023-07-24 23:03:30 +0300

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,7 +6,7 @@ export default function Card({ title, href, active }: { title: string; href: str
        className={`${active && "border-[#5d9fe9ab]"} nextra-card nx-group nx-flex nx-flex-col nx-justify-start nx-overflow-hidden nx-rounded-lg nx-border nx-border-gray-200 nx-text-current nx-no-underline dark:nx-shadow-none hover:nx-shadow-gray-100 dark:hover:nx-shadow-none nx-shadow-gray-100 active:nx-shadow-sm active:nx-shadow-gray-200 nx-transition-all nx-duration-200 hover:nx-border-gray-300 nx-bg-transparent nx-shadow-sm dark:nx-border-neutral-800 hover:nx-bg-slate-50 hover:nx-shadow-md dark:hover:nx-border-neutral-700 dark:hover:nx-bg-neutral-900`}
        href={href}
     >
-      <span class="nx-flex nx-font-semibold nx-items-start nx-gap-2 nx-p-4 nx-text-gray-700 hover:nx-text-gray-900 dark:nx-text-neutral-200 dark:hover:nx-text-neutral-50">
+      <span className="nx-flex nx-font-semibold nx-items-start nx-gap-2 nx-p-4 nx-text-gray-700 hover:nx-text-gray-900 dark:nx-text-neutral-200 dark:hover:nx-text-neutral-50">
         {title}
       </span>
     </Link>
